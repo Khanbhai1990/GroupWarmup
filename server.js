ef6dedbdb23048dcc6be603c64c6f5d79521aca1
Khanbhai1990 <hykhan1@asu.edu>
2017-10-20 10:05:44 -0700

    second commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var fs = require('fs')
+var app = express();
+var port = process.env.PORT || 8000;
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+  // Use res.render to "pass in values" that the HTML document will be able to render
+  // into the template (see below)
+  fs.readFile("./storage.json", 'utf8', function(err, data){
+    let usersArr = JSON.parse(data);
+
+    res.render('index', {pets: usersArr});
+
+  })
+});
